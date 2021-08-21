bb74b0fcfb9ce00740963006d90a31fde78c80d7
FLira2000 <fabioliradev@gmail.com>
2021-08-21 17:26:18 -0300

    gentree basefile

--- Arvore_Genealogica/GenTree.py
+++ Arvore_Genealogica/GenTree.py
@@ -15,25 +15,33 @@ class GenTree:
     def searchLeaf(self, childName) -> Leaf:
         pass
 
-    def insertLeaf(self, parentLeaf, childLeafId = root, whichParent = 0):
+    def insertLeaf(self, parentLeaf, childLeafId = None, whichParent = 0):
         if childLeafId != None:
             self.performInsertion(child = self.searchLeaf(childLeafId), parent=parentLeaf, whichParent=whichParent)
             return
         else:
             self.root = parentLeaf
     
-    def insertLeaf(self, parentLeaf, childLeafName = root, whichParent = 0):
+    def insertLeaf(self, parentLeaf, childLeafName = None, whichParent = 0):
         if childLeafName != None:
             self.performInsertion(child = self.searchLeaf(childLeafName), parent=parentLeaf, whichParent=whichParent)
             return
         else:
             self.root = parentLeaf
     
-    def printTree(self, root: Leaf):
-        root.printMyself()
-        if root.mother != None: 
-            self.printTree(root.mother)
+    def printTree(self, root = None):
+        if root == None:
+            self.root.printMyself()
+            if self.root.mother != None: 
+                self.printTree(self.root.mother)
         
-        if root.father != None: 
-            self.printTree(root.father)
-    
+            if self.root.father != None: 
+                self.printTree(self.root.father)
+        else:
+            root.printMyself()
+            if root.mother != None: 
+               self.printTree(root.mother)
+        
+            if root.father != None: 
+                self.printTree(root.father)
+                
--- Arvore_Genealogica/Leaf.py
+++ Arvore_Genealogica/Leaf.py
@@ -1,6 +1,16 @@
+from random import random
+
 class Leaf:
     UID = None
     name = None
     mother = None
     father = None
+
+    def __init__(self, leafName):
+        self.name = leafName
+        self.UID = round(random() * 100)
+
+    def printMyself(self):
+        print(self.name)
+        print('UID: ' + str(self.UID))
 
