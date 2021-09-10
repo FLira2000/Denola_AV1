5e0f18a314bfbb3e6f799042ebfd0f6c9a73fa4c
Guilherme Portella <fguilhermepll@gmail.com>
2021-09-10 00:04:18 -0300

    Update AVL_Ordering.py

--- AVL_Tree/AVL_Ordering.py
+++ AVL_Tree/AVL_Ordering.py
@@ -57,7 +57,7 @@ class AVL:
             j += 1
         return False
 
-    def insert_word(self, word_val, root):
+    def insert(self, word_val, root):
         if root is None:
             return node(word_val)
         # Then if VALUE is less than [root value], check if the value at left of root is less than VALUE
@@ -82,44 +82,6 @@ class AVL:
             root.right = self.rotateR(root.right)
             return self.rotateL(root)
         return root
-
-
-    #TODO: intersect value as calculation of words alphabetic order,
-    # so, value is the value of alphabetic order of a word
-    # maybe, check value inside function to see if it's higher than the node.value is being compared to
-    def insert(self, val, root):
-        if root is None:
-            return node(val)
-        # Then if VALUE is less than [root value], check if the value at left of root is less than VALUE
-        elif val <= root.value:
-            root.left = self.insert(val, root.left)
-        # Then if VALUE is greater than [root value], check if the value at right of root is greater than VALUE
-        elif val > root.value:
-            root.right = self.insert(val, root.right)
-
-        root.height = 1 + max(self.height(root.left), self.height(root.right))
-        balance = self.balance(root)
-        
-        # if balance is less than -1 and greater than 1, it needs to be balanced
-        if balance > 1 and root.left.value > val:
-            return self.rotateR(root)
-        if balance < -1 and val > root.right.value:
-            return self.rotateL(root)
-        if balance > 1 and val > root.left.value:
-            root.left = self.rotateL(root.left)
-            return self.rotateR(root)
-        if balance < -1 and val < root.right.value:
-            root.right = self.rotateR(root.right)
-            return self.rotateL(root)
-        return root
-
-    # prints the tree as far as I noticed
-    def preorder(self, root):
-        if root is None:
-            return
-        print(root.value)
-        self.preorder(root.left)
-        self.preorder(root.right)
 
     def delete(self, val, Node):
         if Node is None:
@@ -158,41 +120,53 @@ class AVL:
             return self.rotateL(Node)
         return Node
 
-    # TODO: needs work
-    def tree_search(self, word):
-        if self.root is None:
-            return node(val)
-        # Then if VALUE is less than [root value], check if the value at left of root is less than VALUE
-        elif val <= root.value:
-            root.left = self.tree_search(val, root.left)
-        # Then if VALUE is greater than [root value], check if the value at right of root is greater than VALUE
-        elif val > root.value:
-            root.right = self.tree_search(val, root.right)
+def print_tree(node, level=0):
+    if node != None:
+        print_tree(node.left, level + 1)
+        print(' ' * 6 * level + '->', node.value)
+        print_tree(node.right, level + 1)
+
+def count_words_file(path_file):
+    counter = 0
+    with open(path_file,'r') as f:
+        for line in f:
+            for word in line.split():
+                counter +=1
+    return counter
 
 
-def test_normal():
-    Tree = AVL()
-    rt = None
-    rt = Tree.insert(1, rt)
-    rt = Tree.insert(2, rt)
-    rt = Tree.insert(3, rt)
-    rt = Tree.insert(4, rt)
-    rt = Tree.insert(5, rt)
-    print("PREORDER")
-    Tree.preorder(rt)
+def search(root,key):
+    if root is None:
+        return 'Not found' 
+    elif root.value == key:
+        return root.value
+ 
+    # Value is greater than root's key
+    if root.value < key:
+        return search(root.right,key)
+   
+    # Value is smaller than root's key
+    return search(root.left,key)        
 
-def test_word():
-    Tree = AVL()
-    rt = None
-    rt = Tree.insert_word('azul', rt)
-    rt = Tree.insert_word('amarelo', rt)
-    rt = Tree.insert_word('verde', rt)
-    rt = Tree.insert_word('viril', rt)
-    rt = Tree.insert_word('anao', rt)
-    rt = Tree.insert_word('vizil', rt)
-    print("PREORDER")
-    Tree.preorder(rt)
 
-test_word()
-print(' aein ')
-test_normal()
+def return_word(path, index):
+    f = open(path)
+    seperate = list(f.read().split(" "))
+    f.close()
+    return seperate[index]
+
+pathWord = 'C:/Users/usr1/Desktop/words.txt'
+
+Tree = AVL()
+rt = None
+counter = count_words_file(pathWord)
+i = 0
+while(i < counter):
+    print(return_word(pathWord, i))
+    rt = Tree.insert(return_word(pathWord, i), rt)
+    i += 1
+print_tree(rt)
+
+word_search = 'fonfon'
+print('Searching word: ', word_search)
+print('   ', search(rt, word_search))
